b1fe3fdaf213fc4c72f7ee4122ce43020d46c0e5
Thomas Ricard <ricard.thomasj@gmail.com>
2017-04-09 14:22:06 -0400

    working popup menu with vowel types

--- rhyme-react/src/Word.js
+++ rhyme-react/src/Word.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import WordMenu from './WordMenu.js';
 
 class Word extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {showMenu: false};
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    // here you know which component is that, so you can call parent method
+    this.setState((prevState, props) =>({showMenu: prevState.showMenu === false}));
+  }
+
   render() {
     return (
-      <span className="Word">
+      <span className={"Word " +  (this.state.showMenu ? 'active' : '')} onClick={this.onClick}>
         {this.props.text}
+        {this.state.showMenu &&
+          <WordMenu />
+        }
       </span>
+
     );
   }
 }
--- rhyme-react/src/WordMenu.js
+++ rhyme-react/src/WordMenu.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class WordMenu extends Component {
+
+  constructor(props) {
+    super(props);
+    var rhymeTypes =
+    [
+        'AA',
+        'AE',
+        'AH',
+        'AO',
+        'AW',
+        'AY',
+        'EH',
+        'ER',
+        'EY',
+        'IH',
+        'IY',
+        'OW',
+        'OY',
+        'UH',
+        'UW',
+      ];
+    this.state = {rhymeTypes: rhymeTypes};
+  }
+
   render() {
     return (
       <div className="WordMenu">
-
+        <ul>
+          {this.state.rhymeTypes.map((sound, index) =>
+            <li key={index}>{sound}</li>
+          )}
+        </ul>
       </div>
     );
   }
