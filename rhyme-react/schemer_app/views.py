fd6c27f2b1408bdec6899113010b06bb8e2fe27d
Thomas Ricard <ricard.thomasj@gmail.com>
2017-05-30 16:04:29 -0500

    started django app

---
+++ rhyme-react/schemer_app/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'schemer_app/base.html'
